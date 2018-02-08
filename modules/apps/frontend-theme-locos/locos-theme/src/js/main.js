b79f5c006037d81ebc1d7f985d1866326eae050e
Johanna Tchon <johanna.tchon@liferay.com>
2018-02-08 10:18:09 -0800

    rearranged order of imports and converted js to jQuery

--- modules/apps/frontend-theme-locos/locos-theme/src/js/main.js
+++ modules/apps/frontend-theme-locos/locos-theme/src/js/main.js
@@ -32,31 +32,12 @@ Liferay.on(
   */
 
   function() {
+    let $mainContent = $("#wrapper #main-content");
 
     //add animated class to first row of layout
-    var childNodes = document.getElementById('main-content').childNodes;
-    for (var i = 0; i < childNodes.length; i++) {
-      if (childNodes[i].nodeType !== 3) {
-        var firstChild = childNodes[i];
-        var components = firstChild.getElementsByClassName('portlet-content');
-        for (let i = 0; i < components.length; i++) {
-          components[i].className += ' animated';
-        }
-        break;
-      }
-    }
+    let $firstRowPortlets = $mainContent.find('> div:first-child .portlet-content');
+    $firstRowPortlets.addClass('animated');
 
-    // //sticky navigation
-    // window.onscroll = function() {stick()};
-    // var banner = document.getElementById('banner');
-    // var sticky = banner.offsetTop;
-    // function stick() {
-    //   if (window.pageYOffset >= sticky) {
-    //     banner.classList.add('sticky')
-    //   } else {
-    //     banner.classList.remove('sticky');
-    //   }
-    // }
-
+    
   }
 );
