32a3d9e9e8449d68c1b88f5be88926914788343e
Johanna Tchon <johanna.tchon@liferay.com>
2018-02-09 13:44:22 -0800

    added sidebar nav

--- modules/apps/frontend-theme-locos/locos-theme/src/js/main.js
+++ modules/apps/frontend-theme-locos/locos-theme/src/js/main.js
@@ -38,6 +38,55 @@ Liferay.on(
     let $firstRowPortlets = $mainContent.find('> div:first-child .portlet-content');
     $firstRowPortlets.addClass('animated');
 
-    
+    //remove menu button at sign-in and overlay appears
+    // $()
+
+    //create an overlay for banner navigation
+    $('#navigation').prepend('<div id="bars-nav-icon"><i class="icon-align-justify"></i></div>');
+    $('#navigation').prepend('<div id="slide-nav" class="overlay"></div>');
+    $('#slide-nav').append('<a href="javascript:void(0)" class="closebtn">&times;</a>');
+    $('#navigation ul').addClass('overlay-content');
+    $('#slide-nav').append($('#navigation ul'));
+    $('body').append($('#slide-nav'));
+    $('#bars-nav-icon').click(openNav);
+    $('.closebtn').click(closeNav);
+
+    function openNav() {
+      let controlMenuHeight = $('.control-menu').css('height');
+
+      $('.overlay').css('top', controlMenuHeight);
+      $('#slide-nav').css('width', '30%');
+      $('#bars-nav-icon').hide();
+    }
+
+    function closeNav() {
+      $('#slide-nav').css('width', '0');
+      $('#bars-nav-icon').show();
+    }
+
+    //when resize window 
+    $( window ).resize(() => {
+      //responsive overlay
+      let isOverlayPresent = Number($('#slide-nav').css('width').slice(0, -2)) > 0;
+      if(isOverlayPresent) {
+        if (window.innerWidth < 600) {
+          $('#slide-nav').css('width', '100%');
+        } else if (window.innerWidth >= 600) {
+          $('#slide-nav').css('width', '30%');
+        }
+      }
+
+      //every time control menu changes size, reset top for overlay
+      let controlMenuHeight = $('.control-menu').css('height');
+      let overlayTop = $('.overlay').css('top');
+
+      if(isOverlayPresent && controlMenuHeight !== overlayTop) {
+        $('.overlay').css('top', controlMenuHeight);
+      }
+
+    });
+
+
   }
+
 );
