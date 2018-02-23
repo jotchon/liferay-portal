d5f99f9f7d1f0a5e05ed17a5a8e699beb4b61e70
Johanna Tchon <johanna.tchon@liferay.com>
2018-02-23 01:10:19 -0800

    nav shows up to grandchild; file structure of resources importer

--- modules/apps/frontend-theme-locos/locos-theme/src/js/main.js
+++ modules/apps/frontend-theme-locos/locos-theme/src/js/main.js
@@ -31,7 +31,7 @@ Liferay.on(
 	*/
 
 	function() {
-		let $mainContent = $('#wrapper #main-content');
+		// let $mainContent = $('#wrapper #main-content');
 
 		// add animated class to first row of layout
 		// let $firstRowPortlets = $mainContent.find('> div:first-child');
@@ -40,80 +40,118 @@ Liferay.on(
 		// $firstRowPortlets.css('background-color', 'linear-gradient(to bottom, $turquoise 0%, $turquoise 50%, $ivory 50%, $ivory 100%)');
 
 		// a modal for banner navigation
-		function openNav() {
-			let controlMenuHeight = $('.control-menu').css('height') ? $('.control-menu').css('height') : 0;
-
-			$('.modaly').css('top', controlMenuHeight);
-			slideNavCheck();
-			$('.bars-nav-icon').hide();
-			$('#navigation ul').show();
-			$('.background-modaly').show();
-			$('.background-modaly').click(() => {
-				closeNav();
-			});
-		}
-
-		function closeNav() {
-			$('#navigation').css('width', '0');
-			$('.bars-nav-icon').show();
-			$('#navigation ul').hide();
-			$('.background-modaly').hide();
-		}
-
-		let $nav = $('#navigation');
-		$('#banner').append('<div class="bars-nav-icon"><i class="icon-align-justify"></i></div>');
-		$nav.addClass('modaly');
-		$nav.append('<a href="javascript:void(0)" class="closebtn">&times;</a>');
-		$('#navigation > ul').addClass('modaly-content');
-		$('.bars-nav-icon').click(openNav);
-		$('.closebtn').click(closeNav);
-		$('html').append('<div class="background-modaly" ></div>');
-		$('.background-modaly').hide();
-		if (Liferay.ThemeDisplay.isSignedIn()) {
-			$('.bars-nav-icon').show();
-		}
-		else {
-			$('.bars-nav-icon').hide();
-		}
-
-		// when resize window
-		function slideNavCheck() {
-			if (window.innerWidth < 600) {
-				$('#navigation').css('width', '100%');
-			}
-			else if (window.innerWidth >= 600) {
-				$('#navigation').css('width', '30%');
-			}
-		}
-
-		$(window).resize(() => {
-			let modalyPresent = Number($('#navigation').css('width').slice(0, -2)) > 0;
-			// responsive modaly
-			if (modalyPresent) {
+		$( document ).ready(() => {
+			function openNav() {
+				let controlMenuHeight = $('.control-menu').css('height') ? $('.control-menu').css('height') : 0;
+	
+				$('.modaly').css('top', controlMenuHeight);
 				slideNavCheck();
+				$('.bars-nav-icon').hide();
+				$('#navigation ul').show();
+/*				$('.background-modaly').show();
+				// $('.background-modaly').click(() => {
+				// 	closeNav();
+				// });*/
 			}
-
+	
+			function closeNav() {
+				$('#navigation').css('width', '0');
+				$('.bars-nav-icon').show();
+				$('#navigation ul').hide();
+				// $('.background-modaly').hide();
+			}
+	
+			let $nav = $('#navigation');
+			$('#banner').append('<div class="bars-nav-icon"><i class="icon-align-justify"></i></div>');
+			$nav.addClass('modaly');
+			$nav.append('<a href="javascript:void(0)" class="closebtn">&times;</a>');
+			$('#navigation > ul').addClass('modaly-content');
+			$('.bars-nav-icon').click(openNav);
+			$('.closebtn').click(closeNav);
+			// $('html').append('<div class="background-modaly" ></div>');
+			// $('.background-modaly').hide();
+			if (Liferay.ThemeDisplay.isSignedIn()) {
+				$('.bars-nav-icon').show();
+			}
+			else {
+				$('.bars-nav-icon').hide();
+			}
+	
+			// when resize window
+			function slideNavCheck() {
+				if (window.innerWidth < 600) {
+					$('#navigation').css('width', '100%');
+				}
+				else if (window.innerWidth >= 600) {
+					$('#navigation').css('width', '30%');
+				}
+			}
+	
+			$(window).resize(() => {
+				let modalyPresent = Number($('#navigation').css('width').slice(0, -2)) > 0;
+				// responsive modaly
+				if (modalyPresent) {
+					slideNavCheck();
+				}
+	
+				controlMenuCheck();
+			});
+	
+			// adjust top of banner if control menu exists
+			function controlMenuCheck() {
+				let $controlMenuHeight = $('.control-menu').css('height') ? $('.control-menu').css('height') : 0;
+	
+				$('.has-control-menu #banner').css('top', $controlMenuHeight);
+				$('.has-control-menu #navigation').css('top', $controlMenuHeight);
+			}
+	
 			controlMenuCheck();
+
+			$('#navigation > ul > li > i').click(function(event) {
+				let childMenu = $(this).siblings('ul').children('li').children('a');
+				if(childMenu.css('display') === 'none' || childMenu.css('display') === '') {
+					$(this).siblings('a').css('color', '#FFA500');
+					childMenu.css('display', 'block');
+					childMenu.siblings('.icon-chevron-right').length ? childMenu.siblings('.icon-chevron-right').attr('style', 'display: block') : '';
+					$(this).attr('style', 'display: none');
+					$(this).siblings('.icon-chevron-up').attr(
+						'style', 'display: block');
+				}
+				else {
+					$(this).siblings('a').css('color', '#FFFFF0');
+					childMenu.attr('style', 'display: none');
+					childMenu.siblings('.icon-chevron-right').length ? childMenu.siblings('.icon-chevron-right').attr('style', 'display: none') : '';
+					let grandChildren = childMenu.siblings('ul').children('li').children('a');
+					if(grandChildren.css('display') === 'block') {
+						grandChildren.attr('style', 'display: none');
+						childMenu.siblings('.icon-chevron-up').attr('style', 'display: none');
+						childMenu.siblings('.icon-chevron-right').attr('style', 'display: none');
+					}
+					$(this).attr('style', 'display: none');
+					$(this).siblings('.icon-chevron-right').attr('style', 'display: block');
+				}
+
+			});
+
+			$('#navigation > ul > li > ul > li > i').click(function(event) {
+				let grandChildMenu = $(this).siblings('ul').children('li').children('a');
+				if(grandChildMenu.css('display') === 'none' || grandChildMenu.css('display') === '') {
+					$(this).siblings('a').css('color', '#FFA500');
+					grandChildMenu.css('display', 'block');
+					grandChildMenu.siblings('.icon-chevron-right').length ? grandChildMenu.siblings('.icon-chevron-up').attr('style', 'display: block') : '';
+					$(this).attr('style', 'display: none');
+					$(this).siblings('.icon-chevron-up').attr(
+						'style', 'display: block');
+				}
+				else {
+					$(this).siblings('a').css('color', '#FFFFF0');
+					grandChildMenu.css('display', 'none');
+					grandChildMenu.siblings('.icon-chevron-right').length ? grandChildMenu.siblings('.icon-chevron-right').css('display', 'none') : '';
+					$(this).attr('style', 'display: none');
+					$(this).siblings('.icon-chevron-right').attr('style', 'display: block');;
+				}
+			});
 		});
-
-		// mouseover scroll bar appears on gallery only
-		let $galleryContainer = $('.portlet-image-gallery-display .portlet-content-container');
-
-		$galleryContainer.mouseover(() => {
-			$(this).css('overflow-y', 'scroll');
-		});
-		$galleryContainer.mouseout(() => {
-			$(this).css('overflow', 'hidden');
-		});
-
-		// has control menu
-		function controlMenuCheck() {
-			let $controlMenuHeight = $('.control-menu').css('height') ? $('.control-menu').css('height') : 0;
-
-			$('.has-control-menu #banner').css('top', $controlMenuHeight);
-		}
-
-		controlMenuCheck();
 	}
 
 );
